04c5ccd3c77cef6cddebadd8f8cadc2d2032e399
lucaoskaique <lucas.kaique175@hotmail.com>
2023-06-27 12:56:42 -0300

    Fixed robots

--- pages/_document.js
+++ pages/_document.js
@@ -10,13 +10,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
         <meta property="og:url" content="http://lucaskaique.com.br/" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
+        <meta property="og:image" content="https://res.cloudinary.com/lucaos/image/upload/v1687836847/lucaoskaique/pysales_Carnaval_PLANO_2020_-_7_fhn3rb.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@lucaoskaique" />
         <meta name="twitter:image:alt" content="Lucas Kaique - pysales photo"></meta>
