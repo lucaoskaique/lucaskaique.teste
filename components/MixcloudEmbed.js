22eca6fb7c950aad61603d6766ca98e96eb98a79
Lucas Kaíque <lucas.kaique175@hotmail.com>
2023-07-01 00:38:33 -0300

    Created new component

---
+++ components/MixcloudEmbed.js
@@ -0,0 +1,8 @@
+
+const MixcloudEmbed = () => {
+  return (
+    <iframe width="100%" height="120" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Fsensoradio%2Fnonsenso-convida-andrey-lucaos-kaique-est%C3%BAdio-714-senso-radio%2F" frameborder="0" ></iframe>
+  )
+}
+
+export default { MixcloudEmbed }
